cdc6b124ef42ffd95c9fe62bf86d371d77b6d78e
LegnoO <99076232+LegnoO@users.noreply.github.com>
2022-03-27 21:37:39 +0700

    Update baitap.js

--- baitap.js
+++ baitap.js
@@ -20,3 +20,28 @@ function main(){
 }
 
 
+let a = ['JavaScript', 'Python', 'PHP']
+
+Array.prototype.mapA = function (callback) {
+    let output = [],
+        arrayLength = this.length;
+    for (let i = 0; i < arrayLength; i++) {
+        let result = callback(this)
+        output.push(result)
+    }
+    console.log("hehe")
+}
+
+
+
+Array.prototype.foreachA = function (callback) {
+    for (let index in this) {
+        if (this.hasOwnProperty(index)) {
+            callback(this[index], index, this);
+        }
+    }
+}
+
+a.foreachA(function (item, index, array) {
+    console.log(item, index, array)
+}) 
