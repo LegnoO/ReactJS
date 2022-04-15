5646600298e7f9ad11ee1ca0678ab1c475507df3
Legno0 <meogaphieu12@gmail.com>
2022-04-16 00:09:45 +0700

    reactjs_t3h

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Text from './components/commons/Text.js'
+import Button from './components/Button'
+import Input from './components/Button'
+
+import './assets/css/style.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App warp">
+     <Text />
+     <Button />
+     <Input />
     </div>
   );
 }
