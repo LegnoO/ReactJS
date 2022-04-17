9d15c6dfeb7f9c8f2a412033242095e65ecaf5e1
NVinh02 <nguyncvinh129@gmail.com>
2022-04-17 17:43:32 +0700

    Commit project on github

---
+++ src/App.js
@@ -0,0 +1,19 @@
+//import logo from './logo.svg';
+import './App.css';
+import Text from './components/commons/Text.js'
+import Button from './components/Button'
+import Input from './components/Button'
+
+import './assets/css/style.css'
+
+function App() {
+  return (
+    <div className="App warp">
+     <Text />
+     <Button />
+     <Input />
+    </div>
+  );
+}
+
+export default App;
